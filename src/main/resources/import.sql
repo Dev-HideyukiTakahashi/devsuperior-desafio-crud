6654faafed0b834648cd5b22abd368e8e32c0e8c
Hideyuki Takahashi <dev.hideyukitakahashi@gmail.com>
2022-11-17 20:08:57 -0300

    Database seed

---
+++ src/main/resources/import.sql
@@ -0,0 +1,10 @@
+INSERT INTO tb_client (name, cpf, income, birth_date, children) VALUES ('Maria da Silva', '852.283.192.22', 2000.0, TIMESTAMP WITH TIME ZONE'1958-11-23T20:50:07.12345Z', 2);
+INSERT INTO tb_client (name, cpf, income, birth_date, children) VALUES ('João Nascimento', '283.293.084.11', 1500.0, TIMESTAMP WITH TIME ZONE'1985-02-11T21:52:07.12345Z', 0);
+INSERT INTO tb_client (name, cpf, income, birth_date, children) VALUES ('Carlos Nascimento', '823.623.546.77', 200.0, TIMESTAMP WITH TIME ZONE'2010-01-02T20:50:07.12345Z', 0);
+INSERT INTO tb_client (name, cpf, income, birth_date, children) VALUES ('Ana Gomes', '893.850.020-90', 158.0, TIMESTAMP WITH TIME ZONE'1958-11-23T20:50:07.12345Z', 0);
+INSERT INTO tb_client (name, cpf, income, birth_date, children) VALUES ('Domingos de Moraes', '201.182.100-25', 2000.0, TIMESTAMP WITH TIME ZONE'1958-11-23T20:50:07.12345Z', 1);
+INSERT INTO tb_client (name, cpf, income, birth_date, children) VALUES ('Eduarda Matos', '427.153.460-93', 1222.0, TIMESTAMP WITH TIME ZONE'1958-11-23T20:50:07.12345Z', 0);
+INSERT INTO tb_client (name, cpf, income, birth_date, children) VALUES ('Josefa Teixeira', '095.710.720-01', 766.0, TIMESTAMP WITH TIME ZONE'1958-11-23T20:50:07.12345Z', 1);
+INSERT INTO tb_client (name, cpf, income, birth_date, children) VALUES ('Amadeu dos Santos', '658.270.000-60', 4564.0, TIMESTAMP WITH TIME ZONE'1958-11-23T20:50:07.12345Z', 2);
+INSERT INTO tb_client (name, cpf, income, birth_date, children) VALUES ('Anderson Silva', '363.363.070-85', 10000.0, TIMESTAMP WITH TIME ZONE'1958-11-23T20:50:07.12345Z', 0);
+INSERT INTO tb_client (name, cpf, income, birth_date, children) VALUES ('Tom Jobim', '062.282.890-80', 13700.0, TIMESTAMP WITH TIME ZONE'1958-11-23T20:50:07.12345Z', 1);
