511b137a9e736bd103a9b53e9b4ebffc032daa0b
Hideyuki Takahashi <dev.hideyukitakahashi@gmail.com>
2022-11-17 03:45:35 -0300

    Project Created

---
+++ src/main/java/com/devsuperior/crud/DevsuperiorDesafioCrudApplication.java
@@ -0,0 +1,13 @@
+package com.devsuperior.crud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevsuperiorDesafioCrudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevsuperiorDesafioCrudApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/devsuperior/crud/DevsuperiorDesafioCrudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devsuperior.crud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevsuperiorDesafioCrudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
